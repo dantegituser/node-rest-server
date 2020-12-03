a8da79f5c84337c5215a2f387af9263294e735a6
dantegituser <dedisainer@gmail.com>
2020-12-02 21:51:25 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,18 @@
 // PUERTO
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ENTORNO
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// base de datos
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://atlasdatabase01:mongo2021.-@cluster0.bemxx.mongodb.net/test';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,45 +1,19 @@
 require('./config/config');
+
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+app.use(require('./routes/usuario'));
 
-const bodyParser = require('body-parser');
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-app.use(bodyParser.json());
+mongoose.connect(process.env.URLDB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }).then((resp) => { console.log('Connectado a Mongo!!'); })
+    .catch((error) => { console.log('Error connectando a Mongo', error); });
 
-
-
-app.get('/usuario', (req, res) => {
-    // res.send('Hello world');
-    // res.json('Hello world');
-    res.json('Get usuario');
-});
-app.post('/usuario', (req, res) => {
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            body
-        });
-    }
-
-});
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-app.delete('/usuario', (req, res) => {
-
-    res.json('delete usuario');
-});
 app.listen(process.env.PORT, () => {
     console.log('Escuchando en el puerto 3000');
 });
